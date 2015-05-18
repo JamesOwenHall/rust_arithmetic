9b42b26e4b5a6aaeb7806d2b6d3c527d2f7aeb2b
James Hall <james_hall@live.ca>
2015-05-17 20:10:08 -0400

    Evaluates arg[1]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::env;
+
 mod eval;
 mod lex;
 mod parse;
 
 fn main() {
-    let input = " (+ 5 (- (* 2 3 4) (/ 9 3))) ";
-    match eval::evaluate(input) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: ./arithmetic expression");
+        return;
+    }
+
+    match eval::evaluate(&args[1]) {
         Ok(num) => println!("{}", num),
         Err(_) => println!("Error"),
     };
